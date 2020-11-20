c84bac3c0e3095ecaa190053686e784aa11331e1
nikneka <nikitaegorov000@gmail.com>
2020-11-21 00:42:50 +0300

    Initial Commit

---
+++ CHAT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHAT
+//
+//  Created by Никита Егоров on 21.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
